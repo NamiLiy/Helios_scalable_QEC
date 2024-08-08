8850db8e78f0e45ae8175517a75ee9482eadbede
NamiLiy <namithaliyanage@gmail.com>
2024-08-08 13:08:39 -0400

    new root controller

--- design/stage_controller/control_node_root_FPGA.v
+++ design/stage_controller/control_node_root_FPGA.v
@@ -30,7 +30,10 @@ module root_controller #(
 
 `define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-localparam CTRL_MSG_MSB = 47;
+localparam MSG_DEST_MSB = CTRL_FIFO_WIDTH - 1;
+localparam MSG_DEST_LSB = MSG_DEST_MSB - 7;
+localparam MSG_HEADER_MSB = MSG_DEST_LSB - 1;
+localparam MSG_HEADER_LSB = MSG_HEADER_MSB - 7;
 
 
 input clk;
@@ -63,24 +66,29 @@ reg [31:0] cycle_counter;
 reg busy;
 reg odd_clusters;
 
-reg multi_fpga_mode;
 reg measurement_fusion_on;
 
 reg cycle_counter_on;
 reg cycle_counter_reset;
 reg [$clog2(NUM_CHILDREN + 1)-1:0] return_msg_count;
 
-always @(posedge clk) begin
-    if (reset) begin
-        cycle_counter <= 0;
-    end else begin
-        if(cycle_counter_reset) begin
-            cycle_counter <= 2; // to account for propagation time from controller to PEs 
-        end else if(cycle_counter_on) begin
-            cycle_counter <= cycle_counter + 1;
-        end
-    end
-end
+wire [7:0] message_header;
+assign message_header = data_from_cpu [MSG_HEADER_MSB : MSG_HEADER_LSB];
+
+wire [7:0] dest;
+assign message_dest = data_from_cpu [MSG_DEST_MSB : MSG_DEST_LSB];
+
+// always @(posedge clk) begin
+//     if (reset) begin
+//         cycle_counter <= 0;
+//     end else begin
+//         if(cycle_counter_reset) begin
+//             cycle_counter <= 2; // to account for propagation time from controller to PEs 
+//         end else if(cycle_counter_on) begin
+//             cycle_counter <= cycle_counter + 1;
+//         end
+//     end
+// end
 
 localparam DELAY_COUNTER_WIDTH = 8;
 reg [DELAY_COUNTER_WIDTH-1:0] delay_counter;
@@ -95,32 +103,37 @@ always@(posedge clk) begin
     end
 end
 
+
+reg report_all_latencies;
+
 always @(posedge clk) begin
     if (reset) begin
         global_stage <= STAGE_IDLE;
         delay_counter <= 0;
         cycle_counter_on <= 0;
         cycle_counter_reset <= 0;
-        multi_fpga_mode <= 0;
         measurement_fusion_on <= 0;
     end else begin
         case (global_stage)
             STAGE_IDLE: begin // 0
                 if (valid_from_cpu && ready_to_fpgas) begin
-                    if(data_from_cpu [CTRL_MSG_MSB : CTRL_MSG_MSB - 7] == START_DECODING_MSG) begin
-                        multi_fpga_mode <= data_from_cpu [0];
-                        measurement_fusion_on <= data_from_cpu [1];
-                    end else if(data_from_cpu [CTRL_MSG_MSB : CTRL_MSG_MSB - 7] == MEASUREMENT_DATA_HEADER) begin
-                        if(multi_fpga_mode) begin
-                            global_stage <= STAGE_MEASUREMENT_LOADING; // This is to wait reasonable time to load the data
-                        end else if(measurement_fusion_on) begin
-                            if(data_from_cpu[1:0] == 2'b01) begin
-                                global_stage <= STAGE_RESULT_VALID; // The root controller does nothing in single FPGA mode
+                    if(message_dest == 8'h0) begin
+                        case(message_header)
+                            HEADER_INITIALIZE_DECODING: begin
+                                global_stage <= STAGE_MEASUREMENT_LOADING;
                             end
-                        end else begin
-                            global_stage <= STAGE_RESULT_VALID; // The root controller does nothing in single FPGA mode
-                        end
-                        result_valid <= 0;
+
+                            HEADER_SET_BOUNDARIES: begin
+                                global_stage <= STAGE_SET_BOUNDARIES;
+                            end
+
+                            HEADER_DECODE_BLOCK: begin
+                                if(data_from_cpu[2] == 1'b1) begin//wait for result
+                                    global_stage <= STAGE_WAIT_TILL_NODE_RESULTS;
+                                end
+                                report_all_latencies <= data_from_cpu[3];
+                            end
+                        endcase
                     end
                 end
                 iteration_counter <= 0;
@@ -128,95 +141,24 @@ always @(posedge clk) begin
                 cycle_counter_reset <= 1;
                 delay_counter <= 0;
                 return_msg_count <= 0;
-                merge_incomplete <= 0;
-                odd_clusters <= 0;
             end
 
-            STAGE_MEASUREMENT_LOADING: begin
-                // We come to this state only in multi-FPGA mode
-                if(delay_counter < 200) begin
-                    delay_counter <= delay_counter + 1;
-                end else begin
-                    delay_counter <= 0;
-                    global_stage <= STAGE_GROW;
-                end
+            STAGE_SET_BOUNDARIES: begin
+                global_stage <= STAGE_IDLE;
             end
 
-            STAGE_GROW: begin //2
-                // We come to this state only in multi-FPGA mode
-                global_stage <= STAGE_MERGE;
-                cycle_counter_reset <= 0;
-                cycle_counter_on <= 1;
-                iteration_counter <= iteration_counter + 1;
-            end
-
-            STAGE_MERGE: begin //3
-                // We come to this state only in multi-FPGA mode
-                if (delay_counter >= MAXIMUM_DELAY) begin
-                    if(!prelim_busy) begin // Ask nodes for busy states
-                        global_stage <= STAGE_WAIT_TILL_NODE_RESULTS; 
-                    end
-                    delay_counter <= 0;
-                end else begin
-                    delay_counter <= delay_counter + 1;
-                end
-            end
-
-            STAGE_WAIT_TILL_NODE_RESULTS: begin //4
-                // We come to this state only in multi-FPGA mode
-                if (valid_from_fpgas && ready_from_fpgas) begin
-                    if(data_from_fpgas [CTRL_MSG_MSB : CTRL_MSG_MSB - 7] == NODE_RESULT_MSG) begin
+            STAGE_WAIT_TILL_NODE_RESULTS: begin //4  
+                if (valid_from_fpgas && ready_from_fpgas && return_msg_count < NUM_CHILDREN) begin
+                    if(data_from_fpgas [MSG_HEADER_MSB : MSG_HEADER_LSB] == HEADER_RESULT) begin
                         return_msg_count <= return_msg_count + 1;
-                        if(data_from_fpgas [0] == 1'b1) begin
-                            merge_incomplete <= 1'b1;
-                        end else begin
-                            if(data_from_fpgas [1] == 1'b1) begin
-                                odd_clusters <= 1'b1;
-                            end
-                        end
+                        cycle_counter <= data_from_fpgas[15:0];
                     end
-                end
-                if(return_msg_count == NUM_CHILDREN) begin
-                    if(merge_incomplete) begin
-                        global_stage <= STAGE_MERGE;
-                    end else begin
-                        if (odd_clusters) begin
-                            global_stage <= STAGE_GROW;
-                        end else begin
-                            global_stage <= STAGE_PEELING;
-                        end
-                    end
+                end else if(return_msg_count == NUM_CHILDREN && ready_to_cpu) begin
                     return_msg_count <= 0;
-                    merge_incomplete <= 0;
-                    odd_clusters <= 0;
+                    global_stage <= STAGE_IDLE;
                 end
                 
             end           
-
-            STAGE_PEELING: begin //4
-                // We come to this state only in multi-FPGA mode
-                global_stage <= STAGE_RESULT_VALID;
-                cycle_counter_on <= 0;
-            end
-
-            STAGE_RESULT_VALID: begin //5
-                if(multi_fpga_mode == 1'b0) begin
-                    if(valid_from_fpgas) begin
-                        return_msg_count <= return_msg_count + 1;
-                    end
-                    if(return_msg_count == NUM_CHILDREN) begin
-                        global_stage <= STAGE_IDLE;
-                        return_msg_count <= 0;
-                    end
-                end else begin
-                    if(delay_counter >= MAXIMUM_DELAY) begin
-                        global_stage <= STAGE_IDLE;
-                        delay_counter <= 0;
-                    end else begin
-                        delay_counter <= delay_counter + 1;
-                    end
-                end
-            end
             
             default: begin
                 global_stage <= STAGE_IDLE;
@@ -225,59 +167,78 @@ always @(posedge clk) begin
     end
 end
 
+// From CPU to FPGA path
 always@(*) begin
     ready_from_cpu = 1'b0;
-    data_to_cpu = 64'b0;
-    valid_to_cpu = 1'b0;
-
-    ready_from_fpgas = 1'b0;
     data_to_fpgas = 64'b0;
     valid_to_fpgas = 1'b0;
 
     case(global_stage)
         STAGE_IDLE: begin
             ready_from_cpu = ready_to_fpgas;
-            data_to_fpgas = data_from_cpu;
-            valid_to_fpgas = valid_from_cpu;
-        end
+            if (valid_from_cpu && ready_to_fpgas) begin
+                if(message_dest == 8'h0) begin
+                    case(message_header)
+                        //Note that messages indicated to root are converted to broadcast (ff) messages
+                        HEADER_INITIALIZE_DECODING: begin
+                            data_to_fpgas = {8'hff, data_from_cpu [MSG_HEADER_MSB : 0]};
+                            valid_to_fpgas = 1'b1;
+                        end
+                        HEADER_DECODE_BLOCK: begin
+                            data_to_fpgas = {8'hff, data_from_cpu [MSG_HEADER_MSB : 0]};
+                            valid_to_fpgas = 1'b1;
+                        end
 
-        STAGE_GROW: begin
-            data_to_fpgas = {8'hff, 8'hff, MOVE_TO_STAGE, 8'b0, 8'b0, 8'b0, 8'b0, 8'b1};
-            valid_to_fpgas = 1'b1;
-        end
-
-        STAGE_MERGE: begin
-            if (delay_counter >= MAXIMUM_DELAY) begin
-                if(!prelim_busy) begin // Ask nodes for busy states
-                    data_to_fpgas = {8'hff, 8'hff, SEND_ODD_AND_BUSY, 8'b0, 8'b0, 8'b0, 8'b0, 8'b0};
-                    valid_to_fpgas = 1'b1;
+                        default: begin
+                            data_to_fpgas = data_from_cpu;
+                            valid_to_fpgas = 1'b0;
+                        end
+                    endcase
+                end else begin
+                    data_to_fpgas = data_from_cpu;
+                    valid_to_fpgas = 1'b0;
                 end
             end
         end
 
-        STAGE_WAIT_TILL_NODE_RESULTS: begin
-            ready_from_fpgas = 1'b1;
-        end
-
-        STAGE_PEELING: begin
-            data_to_fpgas = {8'hff, 8'hff, MOVE_TO_STAGE, 8'b0, 8'b0, 8'b0, 8'b0, 8'b0};
-            valid_to_fpgas = 1'b1;
-        end
-
-        STAGE_RESULT_VALID: begin
-            if(multi_fpga_mode == 1'b0) begin
-                valid_to_cpu = valid_from_fpgas;
-                data_to_cpu = data_from_fpgas;
-                ready_from_fpgas = ready_to_cpu;
-            end else begin
-                if(delay_counter >= MAXIMUM_DELAY) begin
-                    valid_to_cpu = 1'b1;
-                    data_to_cpu [39:24] = cycle_counter;
-                    data_to_cpu [47:40] = iteration_counter;
-                end 
-            end
+        default: begin
+            data_to_fpgas = data_from_cpu;
+            valid_to_fpgas = 1'b0;
+            ready_from_cpu = 1'b0;
         end
     endcase
+end
+
+// From FPGA to CPU path
+always@(*) begin
+    ready_from_fpgas = 1'b0;
+    data_to_cpu = 64'b0;
+    valid_to_cpu = 1'b0;
+
+    if(report_all_latencies) begin
+        data_to_cpu = data_from_fpgas;
+        valid_to_cpu = valid_from_fpgas;
+        ready_from_fpgas = ready_to_cpu;
+    end else begin
+        case(global_stage)
+            STAGE_WAIT_TILL_NODE_RESULTS: begin
+                if(return_msg_count == NUM_CHILDREN) begin
+                    data_to_cpu = {8'h0,8'h6,16'h0,16'b0,cycle_counter};
+                    valid_to_cpu = 1'b1;
+                    ready_from_fpgas = 1'b0;
+                end else begin
+                    data_to_cpu = data_from_fpgas;
+                    valid_to_cpu = 1'b0;
+                    ready_from_fpgas = 1'b1;
+                end
+            end
+            default: begin
+                data_to_cpu = data_from_fpgas;
+                valid_to_cpu = 1'b0;
+                ready_from_fpgas = 1'b0;
+            end
+        endcase
+    end
 end
 
 endmodule
--- parameters/parameters.sv
+++ parameters/parameters.sv
@@ -14,20 +14,42 @@ localparam [STAGE_WIDTH-1:0]
     STAGE_WRITE_TO_MEM = 8,
     STAGE_WAIT_TILL_NODE_RESULTS = 9,
     STAGE_RESET_ROOTS = 10,
-    STAGE_TEMPORARY = 11;
+    STAGE_TEMPORARY = 11,
+    STAGE_SET_BOUNDARIES = 12;
 
 localparam [7:0]
-    START_DECODING_MSG = 8'h01,
-    MEASUREMENT_DATA_HEADER = 8'h02,
-    SEND_ODD_AND_BUSY = 8'h03,
-    NODE_RESULT_MSG = 8'h04,
-    MOVE_TO_STAGE = 8'h05;
+    HEADER_INITIALIZE_DECODING = 8'h00,
+    HEADER_SET_BOUNDARIES = 8'h01,
+    HEADER_DECODE_BLOCK = 8'h02,
+    HEADER_TRANSFER_ARTIFICIAL_BOUNDARY = 8'h03,
+    HEADER_LOAD_ARTIFICIAL_BOUNDARY = 8'h04,
+    HEADER_REPORT_RESULT = 8'h05,
+    HEADER_RESULT = 8'h06;
 
 //`define DEFINE_DUF_PARAMETERS 1
 //`endif
 
-// START_DECODING_MSG -> 
-// [0] = 0 => single fpga, 1 => multiple fpga
+// Message format | Dest 8 bit | Header 8 bit | Payload 48 bits |
+// Dest 8 bit == 0 is root
+// Dest FF is broadcast
+
+// HEADER_INITIALIZE_DECODING -> NO parameters
+
+// HEADER_SET_BOUNDARIES -> Set boundaries of the bloclk
+// |ID of the group of boundaries|Mask of that boundary|
+// ID goes as -- first and || then
+
+// HEADER_DECODE_BLOCK -> Decode the block
+// |0| indicate peel and finish this block (T and F)
+// |1| indicate report latency at the end of the block (T and F)
+// |2| indicate wait for latency before sending next measurement
+// |3| indicate latency from all FPGAs
+
+// HEADER_RESULT -> Result of the block including decoding latency
+// |15:0| decoding latency
+// |47:42| ID of the sender
+
+
 
 // NODE_RESULT_MSG ->
 // [0] = 1 => busy
